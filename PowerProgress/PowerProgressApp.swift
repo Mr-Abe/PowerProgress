302206680ff9fa882a383c17fecb33de3463a15b
mrabrahams <119377891+Mr-Abe@users.noreply.github.com>
2024-12-31 12:52:59 -0500

    Initial commit

---
+++ PowerProgress/PowerProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerProgressApp.swift
+//  PowerProgress
+//
+//  Created by Dwight Abrahams on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct PowerProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
